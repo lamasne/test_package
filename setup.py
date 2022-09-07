e5eae5699c5a3e32a1d7fe1c531abd11269b4de8
Lamas Neil <neillamas@gmail.com>
2022-09-07 03:06:24 +0200

    added requests to install_requires

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-
 from setuptools import setup
 
 setup(
-    name='test_package',
-    packages=['test'],
-    description='Hello world packaging',
-    version='0.1',
-    url='https://github.com/lamasne/test_package',
-    author='Neil',
-    author_email='neillamas@gmail.com',
-    install_requires=['pandas'],
-    )
+    name="test_package",
+    packages=["test"],
+    description="Hello world packaging",
+    version="0.1",
+    url="https://github.com/lamasne/test_package",
+    author="Neil",
+    author_email="neillamas@gmail.com",
+    install_requires=["requests", "pandas"],
+)
