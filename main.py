7a594893c3bc3a3f1974298a080f337ce09e28cb
Lamas Neil <neillamas@gmail.com>
2022-09-07 02:59:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def my_fct(my_str_list):
+    df = pd.DataFrame(my_str_list)
+    print(df)
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='test_package',
+    packages=['test'],
+    description='Hello world packaging',
+    version='0.1',
+    url='https://github.com/lamasne/test_package',
+    author='Neil',
+    author_email='neillamas@gmail.com',
+    install_requires=['pandas'],
+    )
